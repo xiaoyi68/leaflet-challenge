7afc42c75e732e1500b6b0b39da64dcdd587091d
xiaoyi68 <zhxy@yahoo.com>
2019-10-12 23:02:30 -0500

    hw

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiY20wOTA5MTciLCJhIjoiY2sxODQ2YWs2MW44cjNkbW82YnJwNXYwdiJ9.vlIjB6pQk8_ejjlpbLCG5w";
+const API_KEY = "pk.eyJ1IjoieGlhb3lpNjgiLCJhIjoiY2sxOGlheDF5MDNwdjNicGhid2NkNW9mbSJ9.K6kHlJkzJ5Lbc0RehTE02A";
